25910bdc2908f5feeb5c146630bb573607e55451
nahidplanet <nahidplanet.bd@gmail.com>
2023-01-11 18:31:58 +0600

    meta tag add

--- src/components/Shared/PageTitle.js
+++ src/components/Shared/PageTitle.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
-const PageTitle = ({title}) => {
+const PageTitle = ({ title }) => {
 	return (
 		<Helmet>
-        <title>{title} - Seblem Furniture</title>
-      </Helmet>
+			<meta property="og:title" content="Sublem Furniture || Dubai Furniture" />
+			<meta property="og:type" content="article" />
+			<meta name="description" content="Looking For The Best Furniture Stores in Dubai? Sublem -The Best Places To Get Luxury Furniture In Dubai" />
+			<meta property="og:url" content="https://sublem.com" />
+			<meta property="og:image" content="https://i.ibb.co/ysFttp2/20221220-020248-0000.png" />
+			<title>{title} - Sublem Furniture</title>
+		</Helmet>
 	);
 };
 
