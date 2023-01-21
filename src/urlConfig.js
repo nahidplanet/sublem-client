814b2d631be33a378233e95f6249472a633b1d29
nahidplanet <nahidplanet.bd@gmail.com>
2023-01-21 21:21:58 +0600

    urlConfig setup

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,12 @@
-export const api = 'http://localhost:5000/api/v1';
-export const baseUrl = 'http://localhost:5000/images/product/';
+// =================
+// for local 
+// =================
+// export const api = 'http://localhost:5000/api/v1';
+// export const baseUrl = 'http://localhost:5000/images/product/';
 
-// export const api = 'https://www.admin.sublem.com/api/v1';
-// export const baseUrl = 'https://www.admin.sublem.com/images/product/';
+
+// ===================
+// for production 
+// ===================
+export const api = 'https://www.admin.sublem.com/api/v1';
+export const baseUrl = 'https://www.admin.sublem.com/images/product/';
